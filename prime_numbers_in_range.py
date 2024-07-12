6280fda442c160553c948f0da02f66337bc2f7f1
Anjali23-bit <anjalipasupuleti816@gmail.com>
2024-07-12 15:55:22 +0530

    prime_numbers_in_range.py

--- prime_numbers_in_range.py
+++ prime_numbers_in_range.py
@@ -1,8 +1,8 @@
 n=int(input(" "))
 for num in range(1,n+1):
-if num>1:
-for i in range(2,num):
-if num%i==0:
-break
-else:
-print(num)
+  if num>1:
+    for i in range(2,num):
+      if num%i==0:
+        break
+    else:
+      print(num)
