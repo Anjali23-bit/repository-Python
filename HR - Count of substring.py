e5c9c3c909dd598dcbfcdd4c1a02013a674bcd96
Anjali23-bit <anjalipasupuleti816@gmail.com>
2024-09-28 13:54:27 +0530

    HR - Count of substring.py

---
+++ HR - Count of substring.py
@@ -0,0 +1,14 @@
+string =input()
+substring =input()
+def Count(string,substring):
+    count = 0
+    start = 0
+    while start < len(string):
+        pos = string.find(substring, start)
+        if pos != -1:
+            start = pos + 1
+            count += 1
+        else:
+            break
+    return count
+print(Count(string,substring))
